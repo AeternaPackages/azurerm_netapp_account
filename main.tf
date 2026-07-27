88f1c382dea97bf91f9113726ccafa5957cc6840
Daniel Allen <dan@aeternagroup.net>
2026-07-27 18:30:26 +0100

    Update to schema version v4.81.0

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ locals {
 }
 
 module "netapp_accounts" {
-  source          = "git::https://github.com/AeternaModules/azurerm_netapp_account.git?ref=v4.80.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_netapp_account.git?ref=v4.81.0"
   netapp_accounts = local.netapp_accounts
 }
 
 module "netapp_account_encryptions" {
-  source                     = "git::https://github.com/AeternaModules/azurerm_netapp_account_encryption.git?ref=v4.80.0"
+  source                     = "git::https://github.com/AeternaModules/azurerm_netapp_account_encryption.git?ref=v4.81.0"
   netapp_account_encryptions = local.netapp_account_encryptions
   depends_on                 = [module.netapp_accounts]
 }
